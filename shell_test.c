314e2dacf0ea2d01fc2d8838930c2225e8e90bf3
dmurr <diegocmurray@gmail.com>
2018-08-22 23:40:47 +0000

    Introduced error handler function. Need to remove static num and make global var in input_parse

--- shell_test.c
+++ shell_test.c
@@ -1,5 +1,45 @@
 #include "holberton.h"
 
+void error_handler(char **parsed_input, int errorFlag)
+{
+	/**
+	 * use global var instead of static int num
+	 * num should incremeant on every call not just error
+	 */
+	static int num = 0;
+	char *err_msg;
+	char str;
+	int temp;
+	int i = 0;
+
+	num++;
+/* handles less than 10 subsequent calls */
+	str = num + '0';
+
+/*	if (num < 10)
+		str[0] = num + '0';
+	else
+	{
+		while (num != 0)
+		{
+			temp = num % 10;
+			str[i++] = temp + '0';
+			num = num / 10;
+		}
+	}
+*/
+	switch(errorFlag)
+	{
+	case 1:
+		write(1, "sh: ", 4);
+		write(1, &str, 1);
+		write(1, ": ", 2);
+		write(1, parsed_input[0], sizeof(parsed_input[0]));
+		write(1, ": not found", 11);
+		break;
+	}
+}
+
 /* VERSION 0.1 Only parses on ' '*/
 char **input_parse(char *buffer, char **parsed_input)
 {
@@ -57,7 +97,7 @@ void exec_args(char **parsed_input)
         {
                 if(execve(parsed_input[0], parsed_input, NULL) == -1)
                 {
-                        perror("Error:");
+			error_handler(parsed_input, 1);
                         exit(1);
                 }
         }
