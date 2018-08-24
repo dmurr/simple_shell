9582c5380a262f2a485703aa601f97ad8e474e10
dmurr <diegocmurray@gmail.com>
2018-08-24 23:05:40 +0000

    Implemented path handling

--- shell_test.c
+++ shell_test.c
@@ -25,7 +25,7 @@ int input_get(char *buffer, char **parsed_input)
         size_t size = CHAR_BUF_MAX;
 
         if (getline(&buffer, &size, stdin) == -1)
-                return (1);
+		return (1);
         input_parse(buffer, parsed_input);
         return (0);
 }
@@ -45,7 +45,7 @@ void shell_exec(char **input)
         {
 		if (execve(input[0], input, NULL) == -1)
 		{
-			perror("Error:");
+			perror("**Error:");
 			exit(1);
 		}
 	}
@@ -84,12 +84,82 @@ void shell_exit(char **input)
         }
 }
 
+extern char **environ;
+char *get_path()
+{
+        const char *s = "PATH";
+        int i, j = 0;
+
+        for (i = 0; environ[i][j] != 0; i++)
+        {
+                for (j = 0; j < 4; j++)
+                {
+                        if (s[j] == environ[i][j])
+			{
+				if (j == 3)
+					return (&environ[i][j+2]);
+
+				continue;
+			}
+			else
+                                break;
+                }
+        }
+        return (0);
+}
+
+void process_rel_path(char **parsed_input)
+{
+	char *full_path, *path, *abs_path;
+	char delim[] = {':', '\0'};
+	char **ptr; // shell_exec expects a **ptr... ptr -> abs_path -> /bin/ls
+	int i, j;
+
+	ptr = &abs_path;
+
+	if(!(abs_path = malloc(sizeof(char) * 1024)))
+		return;
+
+	full_path = get_path();
+
+	path = strtok(full_path, delim);
+	while(path)
+	{
+		for(i = 0; path[i] != '\0'; i++)
+			abs_path[i] = path[i];
+		abs_path[i] = '/';
+		i++;
+
+		for (j = 0; parsed_input[0][j] != '\0'; j++)
+		{
+			abs_path[i] = parsed_input[0][j];
+			i++;
+		}
+		abs_path[i] = '\0';
+
+		printf("abs_path = %s\n", abs_path);
+		if (access(abs_path, F_OK) == 0)
+			shell_exec(ptr);
+
+		path = strtok(NULL, delim);
+        }
+	free(abs_path);
+}
+
+void direct_path(char **parsed_input)
+{
+        if (parsed_input[0][0] == '/')
+                shell_exec(parsed_input);
+        else
+                process_rel_path(parsed_input);
+}
+
 void (*input_exec(char **input))(char **)
 {
         exec array[] = {
                 {"exit", shell_exit},
                 {"help", shell_help},
-                {NULL, shell_exec}
+                {NULL, direct_path}
         };
         int i = 0;
 
