dec5c2d6b4c6ebf48fc896f79f12462ed005e671
dmurr <diegocmurray@gmail.com>
2018-08-27 01:20:35 +0000

    Fixing merge conflict. Recreated error handler

--- error_handler.c
+++ error_handler.c
@@ -1,39 +1,35 @@
-void error_handler(char **parsed_input, int errorFlag)
+#include "holberton.h"
+
+void shell_error(struct shell cash, int error)
 {
-	/**
-	 * use global var instead of static int num
-	 * num should incremeant on every call not just error
-	 */
-	static int num = 0;
-	char *err_msg;
-	char str;
-	int temp;
-	int i = 0;
+	char *e_buf, colon[] = {':', ' ', '\0'}, new[] = {'\n', '\0'}, *count;
+	int buf_len = 0;
 
-	num++;
-/* handles less than 10 subsequent calls */
-	str = num + '0';
+	/* Need to initialize malloc memory to 0 */
+	e_buf = malloc(sizeof(char) * CHAR_BUF_MAX);
+	if (e_buf == NULL)
+		exit(1);
 
-/*if (num < 10)
-  str[0] = num + '0';
-  else
-  {
-  while (num != 0)
-  {
-  temp = num % 10;
-  str[i++] = temp + '0';
-  num = num / 10;
-  }
-  }
-*/
-	switch(errorFlag)
+	_strcat(e_buf, cash.name);
+	_strcat(e_buf, colon);
+	count = int_arg(cash.count);
+	_strcat(e_buf, count);
+	_strcat(e_buf, colon);
+	_strcat(e_buf, (cash.p_buf)[0]);
+	_strcat(e_buf, colon);
+	switch(error)
 	{
 	case 1:
-		write(1, "sh: ", 4);
-		write(1, &str, 1);
-		write(1, ": ", 2);
-		write(1, parsed_input[0], sizeof(parsed_input[0]));
-		write(1, ": not found", 11);
+		_strcat(e_buf, "not found\n");
+		write(1, e_buf, _strlen(e_buf));
+		break;
+	case 2:
+		_strcat(e_buf, "Illegal number: ");
+		_strcat(e_buf, (cash.p_buf)[1]);
+		_strcat(e_buf, new);
+		write(1, e_buf, _strlen(e_buf));
 		break;
 	}
+	free(count);
+	free(e_buf);
 }
